5b21b610107678cbfdc2498d1896415369e05521
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2019-11-12 05:55:52 +0700

    tadd NetworkBoundResource class

---
+++ app/src/main/java/com/dargoz/jetpack/data/source/NetworkBoundResource.java
@@ -0,0 +1,7 @@
+package com.dargoz.jetpack.data.source;
+
+import androidx.lifecycle.MediatorLiveData;
+
+public class NetworkBoundResource<ResultType, RequestType> {
+    private MediatorLiveData<Resource<ResultType>> result = new MediatorLiveData<>();
+}
